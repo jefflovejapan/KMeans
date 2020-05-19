f8d904835b9784c7607ab169eb3ad02bca227bd7
Jeffrey Blagdon <jeffblagdon@gmail.com>
2020-05-19 13:42:13 -0300

    Initial Commit

---
+++ KMeans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KMeans
+//
+//  Created by Jeffrey Blagdon on 2020-05-19.
+//  Copyright © 2020 polyergy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
